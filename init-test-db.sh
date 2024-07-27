ba8ff003f3c5307cb6b7f9a09dc4a2ab3c432fc7
thades <thadeshammer@gmail.com>
2024-07-27 13:37:38 -0400

    working postgres DBs (now using the proper user)

--- init-test-db.sh
+++ init-test-db.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-POSTGRES_DB=chatterboxdb
-POSTGRES_USER=$(cat /run/secrets/postgres_user.txt)
-POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password.txt)
+POSTGRES_DB=chatterbox_testdb
+POSTGRES_USER=$(cat /run/secrets/test_postgres_user.txt)
+POSTGRES_PASSWORD=$(cat /run/secrets/test_postgres_password.txt)
 
 psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 EOSQL
 
-echo "User and database created successfully"
+echo "User and test database created successfully"
