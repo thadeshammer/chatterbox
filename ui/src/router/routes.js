2522e282f88a31dc8c561f2a5a254c97faf00bf0
Justin Blau <justin.blau8@gmail.com>
2024-07-30 12:13:53 -0400

    moved ui to root

---
+++ ui/src/router/routes.js
@@ -0,0 +1,19 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/category/:category', component: () => import('pages/CategoryPage.vue')}
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
